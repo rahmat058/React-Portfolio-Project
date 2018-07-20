e14a0b09912bee5ae7e13e209c38e91842fb955b
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-07-20 22:36:13 +0600

    Landing page design is done

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-
+import {Grid, Cell} from 'react-mdl';
 
 class LandingPage extends Component {
    render() {
       return(
-         <div><h1>Landing page</h1></div>
+         <div style={{ width: '100%',  margin:'auto'}}>
+            <Grid className='mdl-grid landing-grid'> 
+               <Cell col={12}>
+                  <img 
+                  src="https://www.shareicon.net/download/2015/09/18/103160_man_512x512.png" 
+                  alt="avatar"
+                  className='avatar-img'/>
+
+                  <div className='banner-text'>
+                     <h1>Full Stack Web Developer</h1>
+                     <hr/>
+                     <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | Vue | Node Js | Express | Mongo DB </p>
+                     <div className='social-links'>
+                        <a href="https://www.google.com" rel='noopener norefferrer' target='_blank'>
+                           <i className='fa fa-google-plus-square' aria-hidden='true'></i>
+                        </a>
+                        <a href="https://www.google.com" rel='noopener norefferrer' target='_blank'>
+                           <i className='fa fa-github-square' aria-hidden='true'></i>
+                        </a>
+                        <a href="https://www.google.com" rel='noopener norefferrer' target='_blank'>
+                           <i className='fa fa-free-code-camp' aria-hidden='true'></i>
+                        </a>
+                        <a href="https://www.google.com" rel='noopener norefferrer' target='_blank'>
+                           <i className='fa fa-youtube-square' aria-hidden='true'></i>
+                        </a>
+                        <a href="https://www.google.com" rel='noopener norefferrer' target='_blank'>
+                           <i className='fa fa-facebook-square' aria-hidden='true'></i>
+                        </a>
+                     </div>
+                  </div>
+               </Cell>
+            </Grid>
+         </div>
       )
    }
 }
